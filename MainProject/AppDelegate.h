a39f0213f384cd98ab19d640227fe8156fc72531
raojb@knowbox.cn <raojb@knowbox.cn>
2018-09-10 10:25:13 +0800

    提交主工程

---
+++ MainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MainProject
+//
+//  Created by rjb on 2018/9/6.
+//  Copyright © 2018年 rjb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MainProject/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  MainProject
+//
+//  Created by rjb on 2018/9/6.
+//  Copyright © 2018年 rjb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
